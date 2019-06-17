33e7fb43f5e2f947a25d111af1e57ec6972e168c
Dustin McRorie <dustin.m@getjobber.com>
2019-06-17 16:54:00 -0600

    Functionality for remembering user login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,9 +5,22 @@ module SessionsHelper
     session[:user] = user.id
   end
 
+  # Remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def current_user
-    if session[:user]
-      @current_user ||= User.find_by(id: session[:user])
+    if (user_id = session[:user])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -17,6 +30,7 @@ module SessionsHelper
   end
 
   def log_out
+    current_user.forget
     session.delete(:user)
     @current_user = nil
   end
